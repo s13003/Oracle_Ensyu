26c26e926757d34aec297b73e7cd196f557fe35b
s13003 <s13003@std.it-college.ac.jp>
2014-11-05 15:04:41 +0900

    sql課題修正

--- 141105-2.sql
+++ 141105-2.sql
@@ -1,13 +1,8 @@
 UPDATE emp_copy
-SET deptno = 20,
-ename = '林'
-WHERE deptno = 1012
+SET ename = '林',
+deptno = 20
+WHERE empno = 1012
 /
 SELECT *
-FROM emp_copy
-/
-ROLLBACK
-/
-select *
 FROM emp_copy
 /

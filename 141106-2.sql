fe6ea2b1b4279cb29be081af75b72e5ccbb9fb76
s13003 <s13003@std.it-college.ac.jp>
2014-11-06 13:47:53 +0900

    sql課題2014-11-06

---
+++ 141106-2.sql
@@ -0,0 +1,13 @@
+INSERT INTO dept_copy(deptno, dname, loc)
+VALUES(70, '海外', '浦添')
+/
+SAVEPOINT save
+/
+INSERT INTO dept_copy(deptno, dname, loc)
+VALUES(80, '製造', 'うるま')
+/
+ROLLBACK TO save
+/
+SELECT *
+FROM dept_copy
+/
---
+++ POINT.sql
@@ -0,0 +1,3 @@
+INSERT INTO dept_copy(deptno, dname, loc)
+VALUES(70, '海外', '浦添')
+/

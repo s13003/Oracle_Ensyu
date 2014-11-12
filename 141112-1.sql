f4322cc0c0444b334b35673dca9f3088ea163fde
s13003 <s13003@std.it-college.ac.jp>
2014-11-12 14:02:40 +0900

    sql課題2014-11-12

---
+++ 141112-1.sql
@@ -0,0 +1,12 @@
+CREATE VIEW v_emp_dept
+AS SELECT empno, ename, dname
+FROM employees JOIN departments
+USING(deptno)
+WHERE deptno = 10
+/
+SELECT *
+FROM v_emp_dept;
+/
+SELECT *
+FROM v_emp_dept
+/
---
+++ 141112-2.sql
@@ -0,0 +1,18 @@
+CREATE VIEW v_emp
+AS empno, ename, sal, deptno
+FROM emp2
+/
+DELETE
+FROM v_emp
+WHERE empno = 1014
+/
+UPDATE v_emp
+SET sal = 300000
+WHERE empno = 1013
+/
+INSERT INTO v_emp(empno, ename, sal, deptno)
+VALUES(1030, '山口', 200000, null)
+/
+SELECT *
+FROM v_emp
+/

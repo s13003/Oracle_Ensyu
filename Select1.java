a2722c7a30e7d2c75c321439179b150451944309
s13003 <s13003@std.it-college.ac.jp>
2014-11-18 15:15:08 +0900

    sql課題提出2014-11-18

---
+++ Select1.java
@@ -0,0 +1,66 @@
+import java.sql.*;
+
+public class Select1 {
+  private String _user = "s13003";
+  private String _pass = "password";
+  private String _host = "172.16.40.4";
+  private String _sid = "db11";
+
+  public static void main(String[] args){
+
+    Select1 sample = new Select1();
+    try {
+      sample.select1();
+    }catch(Exception e){
+      e.printStackTrace();
+    }
+  }
+
+  private void select1() throws Exception{
+    Connection conn = null;
+    Statement st = null;
+    ResultSet rs = null;
+    PreparedStatement ps = null;
+
+    try {
+      Class.forName("oracle.jdbc.driver.OracleDriver");
+      conn = DriverManager.getConnection(
+          "jdbc:oracle:thin:@" + _host + ":1521:" + _sid, _user, _pass);
+
+      st = conn.createStatement();
+
+      ps = conn.prepareStatement("SELECT e.empno, e.ename, e.job, m.ename, d.dname, d.loc FROM employees e LEFT OUTER JOIN employees m ON e.mgr = m.empno JOIN departments d ON e.deptno = d.deptno");
+
+      rs = ps.executeQuery();
+
+      while(rs.next()){
+        String e_empno = rs.getString(1);
+        String e_ename = rs.getString(2);
+        String e_job = rs.getString(3);
+        String m_ename = rs.getString(4);
+        String d_dname = rs.getString(5);
+        String d_loc = rs.getString(6);
+
+        System.out.printf("社員番号： %s\t社員名： %s\t職種:  %s\t上司の名前: %s\t部署名: %s\t場所 %s\n", e_empno, e_ename, e_job, m_ename, d_dname, d_loc);
+      }
+    }catch(ClassNotFoundException e){
+      throw e;
+    }catch(SQLException e){
+      throw e;
+    }catch(Exception e){
+      throw e;
+    }finally{
+      if(conn != null){
+        conn.close();
+      }
+      if(st != null){
+        st.close();
+        st = null;
+      }
+      if(rs != null){
+        rs.close();
+        rs = null;
+      }
+    }
+  }
+}

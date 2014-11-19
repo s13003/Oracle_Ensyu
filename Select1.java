8986f2973bfa1f8adfe515676f3b8a736d66d02c
s13003 <s13003@std.it-college.ac.jp>
2014-11-19 15:17:47 +0900

    sql課題修正

--- Select1.java
+++ Select1.java
@@ -10,7 +10,6 @@ public class Select1 {
 
   public static void main(String[] args){
 
-      System.out.print("社員番号を入力してください:");
     Select1 sample = new Select1();
     try {
       sample.select1();
@@ -32,6 +31,25 @@ public class Select1 {
 
       st = conn.createStatement();
 
+      
+ps = conn.prepareStatement("SELECT e.empno, e.ename, e.job, m.ename, d.dname, d.loc, e.sal, s.grade FROM employees e LEFT JOIN employees m ON e.mgr = m.empno JOIN departments d ON e.deptno = d.deptno JOIN salgrades s ON e.sal BETWEEN s.losal AND s.hisal");
+
+      rs = ps.executeQuery();
+      
+      while(rs.next()){
+        String e_empno = rs.getString(1);
+        String e_ename = rs.getString(2);
+        String e_job = rs.getString(3);
+        String m_ename = rs.getString(4);
+        String d_dname = rs.getString(5);
+        String d_loc = rs.getString(6);
+        String e_sal = rs.getString(7);
+        String s_grade = rs.getString(8);
+       
+        System.out.printf("社員番号： %s\t社員名： %s\t職種:  %s\t上司の名前: %s\t部署名: %s\t場所 %s\t給与 %s\t給与の等級  %s\n", e_empno, e_ename, e_job, m_ename, d_dname, d_loc, e_sal, s_grade);
+      }
+
+      System.out.print("社員番号を入力してください:");
       ps = conn.prepareStatement("SELECT e.empno, e.ename, e.job, m.ename, d.dname, d.loc, e.sal, s.grade FROM employees e LEFT JOIN employees m ON e.mgr = m.empno JOIN departments d ON e.deptno = d.deptno JOIN salgrades s ON e.sal BETWEEN s.losal AND s.hisal WHERE e.empno = ?");
 
       int empno = stdIn.nextInt();

14d7cb7a86a46061d5ce662f4ac8604bc5a677b3
s13003 <s13003@std.it-college.ac.jp>
2014-11-20 14:47:33 +0900

    sql課題提出2014-11-19

--- Select1.java
+++ Select1.java
@@ -23,6 +23,8 @@ public class Select1 {
     Statement st = null;
     ResultSet rs = null;
     PreparedStatement ps = null;
+    boolean employeesExist = false;
+    boolean SubExist = false;
 
     try {
       Class.forName("oracle.jdbc.driver.OracleDriver");
@@ -37,6 +39,7 @@ ps = conn.prepareStatement("SELECT e.empno, e.ename, e.job, m.ename, d.dname, d.
       rs = ps.executeQuery();
       
       while(rs.next()){
+        employeesExist = true;
         String e_empno = rs.getString(1);
         String e_ename = rs.getString(2);
         String e_job = rs.getString(3);
@@ -48,7 +51,6 @@ ps = conn.prepareStatement("SELECT e.empno, e.ename, e.job, m.ename, d.dname, d.
        
         System.out.printf("社員番号： %s\t社員名： %s\t職種:  %s\t上司の名前: %s\t部署名: %s\t場所 %s\t給与 %s\t給与の等級  %s\n", e_empno, e_ename, e_job, m_ename, d_dname, d_loc, e_sal, s_grade);
       }
-
       System.out.print("社員番号を入力してください:");
       ps = conn.prepareStatement("SELECT e.empno, e.ename, e.job, m.ename, d.dname, d.loc, e.sal, s.grade FROM employees e LEFT JOIN employees m ON e.mgr = m.empno JOIN departments d ON e.deptno = d.deptno JOIN salgrades s ON e.sal BETWEEN s.losal AND s.hisal WHERE e.empno = ?");
 
@@ -67,8 +69,32 @@ ps = conn.prepareStatement("SELECT e.empno, e.ename, e.job, m.ename, d.dname, d.
         String s_grade = rs.getString(8);
 
        
-        System.out.printf("社員番号： %s\t社員名： %s\t職種:  %s\t上司の名前: %s\t部署名: %s\t場所 %s\t給与 %s\t給与の等級  %s\n", e_empno, e_ename, e_job, m_ename, d_dname, d_loc, e_sal, s_grade);
+        System.out.printf("社員番号： %s\t社員名： %s\t職種:  %s\t上司の名前: %s\t部署名: %s\t場所 %s\t給与 %s\t給与の等級  %s\n", e_empno, e_ename, e_job, e_ename, d_dname, d_loc, e_sal, s_grade);
       }
+
+      ps = conn.prepareStatement("SELECT e.empno, e.ename, e.job, d.dname, d.loc, e.sal, s.grade FROM employees e JOIN departments d ON e.deptno = d.deptno JOIN salgrades s ON e.sal BETWEEN s.losal AND s.hisal WHERE e.mgr = ?");
+
+      ps.setInt(1, empno);
+      rs = ps.executeQuery();
+      
+      System.out.println("↓部下たち↓");
+      while(rs.next()){
+        SubExist = true;
+        String e_empno = rs.getString(1);
+        String e_ename = rs.getString(2);
+        String e_job = rs.getString(3);
+        String d_dname = rs.getString(4);
+        String d_loc = rs.getString(5);
+        String e_sal = rs.getString(6);
+        String s_grade = rs.getString(7);
+
+       
+        System.out.printf("社員番号： %s\t社員名： %s\t職種:  %s\t部署名: %s\t場所 %s\t給与 %s\t給与の等級  %s\n", e_empno, e_ename, e_job,  d_dname, d_loc, e_sal, s_grade);
+      }
+      if (employeesExist && !SubExist) {
+        System.out.println("部下はいません。");
+      }
+
     }catch(ClassNotFoundException e){
       throw e;
     }catch(SQLException e){
